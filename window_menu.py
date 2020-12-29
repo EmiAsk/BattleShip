2af18b4155bbf3415923808b65a732476ce6f0b4
drammma <Safonova14092004@yandex.ru>
2020-12-30 01:13:29 +0400

    update window_menu

--- window_menu.py
+++ window_menu.py
@@ -21,11 +21,11 @@ def load_image(name, colorkey=None):
 class Button:
     pygame.init()
     button_sound = pygame.mixer.Sound('click_button.wav')
-    font = pygame.font.Font('segoepr.ttf', 20)
+    font = pygame.font.Font('segoepr.ttf', 22)
     def __init__(self, width, height):
         self.width = width
         self.heigth = height
-        self.inactive_color = (100, 100, 255)
+        self.inactive_color = (100, 149, 237)
         self.active_color = (50, 50, 255)
 
     def draw(self, x, y, message, function=None):
@@ -45,7 +45,7 @@ class Button:
         else:
             pygame.draw.rect(screen, self.inactive_color, (x, y, self.width, self.heigth))
             pygame.draw.rect(screen, 'black', (x, y, self.width, self.heigth), 1)
-        self.print_text(message, x + 10, y + 10)
+        self.print_text(message, x + 5, y + 5)
 
     def print_text(self, message, x, y):
         self.text = self.font.render(message, True, [0, 0, 0])
@@ -56,17 +56,22 @@ class Button:
 
 
 class Menu:
+    pygame.font.init()
+    font = pygame.font.Font('segoepr.ttf', 42)
     def __init__(self, screen):
-        pass
+        self.system_labels()
 
-
+    def system_labels(self):
+        pygame.draw.rect(screen, (100, 149, 237), (0, 20, width, 80))
+        self.text = self.font.render('MAIN MENU', True, [0, 0, 0])
+        screen.blit(self.text, (270, 25))
 
 
 if __name__ == '__main__':
     pygame.init()
     pygame.mixer.init()
-
-    screen = pygame.display.set_mode((500, 500))
+    size = width, height = 800, 700
+    screen = pygame.display.set_mode(size)
     image = load_image('fon.jpg')
     screen.blit(image, (0, 0))
     pygame.display.flip()
@@ -77,25 +82,26 @@ if __name__ == '__main__':
 
 
     while running:
-        # Menu(screen)
+        Menu(screen)
 
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        button_account = Button(200, 50)
-        button_account.draw(155, 100, 'Personal Account')
+        button_size = (240, 50)
+        button_account = Button(button_size[0], button_size[1])
+        button_account.draw((height / 2) - (button_size[0] / 4) - 10, 200, '  Personal Account')
 
-        button_game = Button(200, 50)
-        button_game.draw(155, 180, '    New Game')
+        button_game = Button(240, 50)
+        button_account.draw((height / 2) - (button_size[0] / 4) - 10, 300, '      New Game')
 
-        button_settings = Button(200, 50)
-        button_settings.draw(155, 260, '      Settings')
+        button_settings = Button(240, 50)
+        button_account.draw((height / 2) - (button_size[0] / 4) - 10, 400, '        Settings')
 
-        button_records = Button(200, 50)
-        button_records.draw(155, 340, ' Table of Records')
+        button_records = Button(240, 50)
+        button_account.draw((height / 2) - (button_size[0] / 4) - 10, 500, '   Table of Records')
 
         button_exit = Button(200, 50)
-        button_exit.draw(155, 420, '       Logout')
+        button_account.draw((height / 2) - (button_size[0] / 4) - 10, 600, '        Logout')
     pygame.display.flip()
