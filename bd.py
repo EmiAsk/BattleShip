a025a419e02e2fc1cc7edeb1641e5a400659fc4f
lisoka <74776905+lisoka@users.noreply.github.com>
2021-01-16 20:32:00 +0400

    debug

--- bd.py
+++ bd.py
@@ -1,25 +1,25 @@
 import sqlite3
 con = sqlite3.connect('battleship.bd')
 cur = con.cursor()
-cur.execute("UPDATE games SET result = ? WHERE name = ?", (config.RESULT, config.NAME))
+cur.execute("UPDATE games SET result = ? WHERE name = ?", (config.RESULT, config.USER_NAME))
 
-cur.execute("UPDATE games SET score = ? WHERE name = ?", (config.SCORE, config.NAME))
+cur.execute("UPDATE games SET score = ? WHERE name = ?", (config.SCORE, config.USER_NAME))
 
 wins = cur.execute("SELECT wins FROM users WHERE duration = ''").fetchall()
 for elem in wins:
     wins = elem[0]
-cur.execute("UPDATE users SET wins = ? WHERE name = ?", (wins + 1, config.NAME))
+cur.execute("UPDATE users SET wins = ? WHERE name = ?", (wins + 1, config.USER_NAME))
 
 lose = cur.execute("SELECT lose FROM users WHERE duration = ''").fetchall()
 for elem in lose:
     lose = elem[0]
-cur.execute("UPDATE users SET lose = ? WHERE name = ?", (lose + 1, config.NAME))
+cur.execute("UPDATE users SET lose = ? WHERE name = ?", (lose + 1, config.USER_NAME))
 
 score = cur.execute("SELECT score FROM users WHERE duration = ''").fetchall()
 for elem in score:
     score = elem[0]
 if config.SCORE > score:
-    cur.execute("UPDATE users SET best_result (score) = ? WHERE name = ?", (config.SCORE, config.NAME))
+    cur.execute("UPDATE users SET best_result (score) = ? WHERE name = ?", (config.SCORE, config.USER_NAME))
 
 con.commit()
 con.close()
